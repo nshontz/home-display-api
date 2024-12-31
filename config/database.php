0ccedf63d447f7f98b9461199059925da1d20947
Nick Shontz <nick.shontz@yakimachief.com>
2024-12-30 21:26:34 -0700

    postgres config

--- config/database.php
+++ config/database.php
@@ -75,7 +75,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require'
+            'sslmode' => env('SSL_MODE','verify-ca'),
+            'sslrootcert' => env('PGSQL_SSL_CA','/var/www/html/ca-certificate.crt'),
         ],
 
         'sqlsrv' => [
@@ -125,7 +126,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
