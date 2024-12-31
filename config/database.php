3ef9533f1c97b311cab9d1d9b3a71c11b9a59d61
Nick Shontz <nick.shontz@yakimachief.com>
2024-12-30 22:02:24 -0700

    postgres config

--- config/database.php
+++ config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('SSL_MODE','verify-ca'),
+            'sslmode' => env('SSL_MODE','require'),
             'sslrootcert' => env('PGSQL_SSL_CA','/var/www/certs/ca-certificate.crt'),
         ],
 
